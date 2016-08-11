4aa7ff5fa7ccd46d297d3a4ccb1cbe67cd67240d
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2016-08-11 15:16:23 +0000

    Add some optimizations for optimized run comparisment

---
+++ optimize.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dir=$(dirname $(readlink -f $0))
+path=$(basename $dir)
+
+cd $dir/magento/
+
+# Disable modules that are config disabled by default in Magento 1.x, but enabled by default in 2.x
+bin/magento module:disable Magento_Swatches
+
+# Disable modules that we don't need for a load test scenario,
+# in Magento 1.x it was not possible out of the box
+bin/magento module:disable Magento_Ups
+bin/magento module:disable Magento_Usps
+bin/magento module:disable Magento_Dhl
+bin/magento module:disable Magento_Paypal
+bin/magento module:disable Magento_Authorizenet
+bin/magento module:disable Magento_Braintree
+
+rm -rf var/di var/generation var/cache 
+
+bin/magento cache:flush
+bin/magento setup:upgrade
+bin/magento setup:di:compile
+bin/magento setup:static-content:deploy
